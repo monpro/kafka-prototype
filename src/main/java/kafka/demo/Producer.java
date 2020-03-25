584b9a4563671af0d763ed83dca16d43763f5c7b
monpro <monproshen@gmail.com>
2020-03-25 23:44:15 +1100

    set up of producer

--- src/main/java/kafka/demo/Producer.java
+++ src/main/java/kafka/demo/Producer.java
@@ -1,7 +1,35 @@
 package kafka.demo;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class Producer {
     public static void main(String[] args) {
-        System.out.println("producer");
+        //define Producer properties
+        Properties properties = new Properties();
+        String bootstrapServers = "127.0.0.1:9092";
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic", "test_message");
+
+        //send data - async
+        producer.send(record);
+
+        producer.flush();
+
+        producer.close();
+
+
     }
 }
