049191d0a642696f376093ae4f9d46427c566663
monpro <monproshen@gmail.com>
2020-03-26 18:17:01 +1100

    async callbacks to illustrate round-robin

--- src/main/java/kafka/demo/Producer.java
+++ src/main/java/kafka/demo/Producer.java
@@ -1,15 +1,17 @@
 package kafka.demo;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class Producer {
     public static void main(String[] args) {
         //define Producer properties
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
+
         Properties properties = new Properties();
         String bootstrapServers = "127.0.0.1:9092";
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -19,12 +21,28 @@ public class Producer {
         //create the producer
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        for(int i = 0; i < 10; i++) {
 
-        ProducerRecord<String, String> record =
-                new ProducerRecord<String, String>("first_topic", "test_message");
 
-        //send data - async
-        producer.send(record);
+            final ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>("first_topic", "test_message" + Integer.toString(i));
+
+            //send data - async
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        //
+                        logger.info("Received new metaData. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "TimeStamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("error for producer", e);
+                    }
+                }
+            });
+        }
 
         producer.flush();
 
