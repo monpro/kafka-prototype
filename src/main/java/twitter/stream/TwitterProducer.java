6b310ad1a02a7a353631728361177b3a257c653f
monpro <monproshen@gmail.com>
2020-03-27 13:43:17 +1100

    generate stream data from twitter client

--- src/main/java/twitter/stream/TwitterProducer.java
+++ src/main/java/twitter/stream/TwitterProducer.java
@@ -10,12 +10,21 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
+
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+    String consumerKey = "JIQ2EM3ObxKoZzBECKdVKvZLN";
+    String consumerSecret = "30fxYp7ogCSpLN7hUTLQwNAYOlXAM4WQmsnEjGov0kkNcFczQg";
+    String token = "1064278418798272512-YdOLOQjUAianiHu4OFmDkzbBA2nVEp";
+    String secret = "BLKmnLKbCbRpGsSXNIjF5zGlXQrr9209MI06yOfhbGmiX";
 
     private TwitterProducer(){
 
@@ -26,28 +35,41 @@ public class TwitterProducer {
     }
 
     private void run(){
+        logger.info("setup");
+
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
         //create a twitter client
-        Client twitterClient = createTwitterClient();
+        Client twitterClient = createTwitterClient(msgQueue);
         //create a kafka producer
         twitterClient.connect();
         //send tweets to kafka
+
+        while (!twitterClient.isDone()){
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if(msg != null){
+                logger.info(msg);
+            }
+
+        }
+
+        logger.info("end");
     }
 
 
-    String consumerKey = "JIQ2EM3ObxKoZzBECKdVKvZLN";
-    String consumerSecret = "30fxYp7ogCSpLN7hUTLQwNAYOlXAM4WQmsnEjGov0kkNcFczQg";
-    String token = "1064278418798272512-YdOLOQjUAianiHu4OFmDkzbBA2nVEp";
-    String secret = "BLKmnLKbCbRpGsSXNIjF5zGlXQrr9209MI06yOfhbGmiX";
-
-    public Client createTwitterClient(){
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+    public Client createTwitterClient(BlockingQueue<String> msgQueue){
 
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
         Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
 
         // Optional: set up some followings and track terms
-        List<String> terms = Lists.newArrayList("kafka");
+        List<String> terms = Lists.newArrayList("bitcoin");
         hosebirdEndpoint.trackTerms(terms);
 
         // These secrets should be read from a config file
