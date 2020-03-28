d46e54d62684a35a4b53bca50d7a10d604468457
monpro <monproshen@gmail.com>
2020-03-28 16:59:33 +1100

    set up es search client

--- kafka-twitter-consumer/src/main/java/com.github.consumer/ElasticSearchConsumer.java
+++ kafka-twitter-consumer/src/main/java/com.github.consumer/ElasticSearchConsumer.java
@@ -6,16 +6,23 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class ElasticSearchConsumer {
 
     public static RestHighLevelClient createClient(){
-        String hostname = "";
-        String username = "";
-        String password = "";
+        String hostname = Constants.hostname;
+        String username = Constants.username;
+        String password = Constants.password;
 
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 
@@ -35,7 +42,22 @@ public class ElasticSearchConsumer {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(ElasticSearchConsumer.class.getName());
+
+        RestHighLevelClient client = createClient();
+        String jsonString = "{\"test\" : \"result\"}";
+        IndexRequest indexRequest = new IndexRequest(
+                "twitter",
+                "tweets"
+        ).source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexResponse.getId();
+
+        logger.info(id);
+
+        client.close();
 
     }
 
