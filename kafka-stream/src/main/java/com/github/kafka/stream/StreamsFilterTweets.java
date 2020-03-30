1fe1f25d6c4a70d642bf680577795870ee157fa1
monpro <monproshen@gmail.com>
2020-03-30 11:26:24 +1100

    set Properties and add streambuilder

--- kafka-stream/src/main/java/com/github/kafka/stream/StreamsFilterTweets.java
+++ kafka-stream/src/main/java/com/github/kafka/stream/StreamsFilterTweets.java
@@ -1,11 +1,28 @@
 package com.github.kafka.stream;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
 public class StreamsFilterTweets {
     public static void main(String[] args) {
         //create properties
 
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams");
+        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
 
         // create a topology
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, String> inputTopic = streamsBuilder.stream("twitter_topics");
+
 
         //
     }
