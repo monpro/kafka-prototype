03a035f529e3b27861b9c3c277ec62324d8de40a
monpro <monproshen@gmail.com>
2020-03-30 11:23:06 +1100

    init project

---
+++ kafka-stream/src/main/java/com/github/kafka/stream/StreamsFilterTweets.java
@@ -0,0 +1,12 @@
+package com.github.kafka.stream;
+
+public class StreamsFilterTweets {
+    public static void main(String[] args) {
+        //create properties
+
+
+        // create a topology
+
+        //
+    }
+}
